eedbc8b1b79b47cc23f744ee43b5ca9b7cc90343
Anthony <gachinigaithuma@gmail.com>
2023-10-15 19:19:14 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+function multiplier(){
+   
+    return multiply;
+}
+    let random = Math.ceil(Math.random())
+
+function randomNumber(){
+
+    return random;
+}
+let num3 = 32
+let num4 = 7
+
+let mod = num3 % num4
+
+function quotient(){
+
+    return mod;
+}
+
+let max = Math.max(1,2,4,12,20)
+
+max;
